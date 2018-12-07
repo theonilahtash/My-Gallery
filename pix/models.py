b18d91c1dc0c6cf5645d6b3299a9987d868c8ebb
Theonilah Owali <theonilahtash@gmail.com>
2018-12-07 08:22:52 +0300

    added a class image with features like name,category,location

--- pix/models.py
+++ pix/models.py
@@ -1,4 +1,27 @@
 from django.db import models
+import datetime as dt
+
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+    def save_category(self):
+        self.save()
+
+class Image(models.Model):
+    image = models.ImageField(upload_to='images/')
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+        return self.name
+
+    def save_image(self):
+        self.save()
 
