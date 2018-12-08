33ef32fc521bb6f6cdb40a8dc60ab0a6a21e6c81
Theonilah Owali <theonilahtash@gmail.com>
2018-12-08 13:16:09 +0300

    added a location to the views to get the image location by id

--- pix/views.py
+++ pix/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse,Http404
 import datetime as dt
-from .models import Image
+from .models import Image,Location
 
 
 # Create your views here.
@@ -16,39 +16,48 @@ def photos(request):
     return render(request, 'all-photos/today-photos.html', {"date": date,"photos":photos})
 
 
-def search_results(request):
-  
-    if 'image' in request.GET and request.GET["image"]:
-        category = request.GET.get("image")
-        searched_categories = Image.search_image(category)
-        message = f"{category}"
+def convert_dates(dates):
+    
+    # Function that gets the weekday number for the date.
+    day_number = dt.date.weekday(dates)
 
-        return render(request, 'all-photos/search.html',{"message":message,"images": searched_categories})
+    days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Saturday',"Sunday"]
+
+    # Returning the actual day of the week
+    day = days[day_number]
+    return day 
+
+# view function for present posts from past days
+def past_days_photos(request,past_date):
+    try:
+    
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
+      
+    except ValueError:
+        # Raise 404 error when ValueError is thrown    
+        raise Http404()
+        assert False
+
+    if date == dt.date.today():
+        return redirect(photos_today)
+
+    photos = Image.days_photos(date)    
+    return render(request, 'all-photos/past-photos.html', {"date": date, "photos": photos})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'all-photos/search.html', {"message": message, "images": searched_images}) 
 
     else:
-        message = " Found 0 images for the search term"
-        return render(request, 'all-photos/search.html',{"message":message})
+        message = "Found 0 searched image"
+        return render(request, 'all-photos/search.html', {"message": message}) 
 
-#     convert_dates(dates)
-#     # Function that gets weekday number for the dates.
-#     day_number = dt.date.weekday(dates)
-#     days = ['Monday','Tuesday','Wednesday','Thursday','Friday','Sarturday',"Sunday"]
-
-#     # Returning the actual day of the week
-#     day = days[day_number]
-#     return day
-
-# def past_days_photos(request,past_date):
-#     try:
-#         # Converts data from the string Url
-#         date = dt.datetime.strptime(past_date,'%Y-%m-%d').date()
-#     except ValueError:
-#         # Raise 404 error when ValueError is thrown
-#         raise Http404()
-
-#     if date == dt.date.today():
-#         return redirect(photos_of_day)
-
-#     return render(request,'all-photos/past-photos.html',{"date":date})
-
-      
+def location(request,location_id):
+    image =  Image.objects.filter(location_id = location_id)
+    return render(request, 'all-photos/locations.html', {"location":image})
